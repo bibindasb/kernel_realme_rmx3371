d4ccf95ac0745ca1fb924dd6a92c9d1606df5981
Martin Liu <liumartin@google.com>
2020-02-12 23:21:10 +0800

    Revert "psi: Add emergency trigger"
    This reverts commit a7517e81e239ef81d2d7e2cc416b65d0042a52c8.
    
    Reason: revert non upstream code
    Bug: 148903434
    Test: boot
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I17ef7ebfcf9cbee16e4b771cb5aa75bbaf11b4a1
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/psi.h
+++ include/linux/psi.h
@@ -23,7 +23,10 @@ void psi_memstall_leave(unsigned long *flags);
 
 int psi_show(struct seq_file *s, struct psi_group *group, enum psi_res res);
 
-void psi_emergency_trigger(void);
+#ifdef CONFIG_CGROUPS
+int psi_cgroup_alloc(struct cgroup *cgrp);
+void psi_cgroup_free(struct cgroup *cgrp);
+void cgroup_move_task(struct task_struct *p, struct css_set *to);
 
 struct psi_trigger *psi_trigger_create(struct psi_group *group,
 			char *buf, size_t nbytes, enum psi_res res);
@@ -44,8 +47,6 @@ static inline void psi_init(void) {}
 
 static inline void psi_memstall_enter(unsigned long *flags) {}
 static inline void psi_memstall_leave(unsigned long *flags) {}
-
-static inline void psi_emergency_trigger(void){}
 
 #ifdef CONFIG_CGROUPS
 static inline int psi_cgroup_alloc(struct cgroup *cgrp)
