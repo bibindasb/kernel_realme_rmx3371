70d4b33ae464683c246763acf1b34b8ae5b10e7e
Park Ju Hyung <qkrwngud825@gmail.com>
2021-04-04 16:57:12 +0900

    zram: show deduped status in debugfs
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: If349e68ff0b53c4b25dc53f170a6ea70be35f610

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -52,16 +52,7 @@ enum zram_pageflags {
 	ZRAM_UNDER_WB,	/* page is under writeback */
 	ZRAM_HUGE,	/* Incompressible page */
 	ZRAM_IDLE,	/* not accessed page since last idle marking */
-#ifdef CONFIG_HYBRIDSWAP_ASYNC_COMPRESS
-	ZRAM_CACHED,   /* page is cached in async compress cache buffer */
-	ZRAM_CACHED_COMPRESS, /* page is under async compress */
-#endif
-#ifdef CONFIG_HYBRIDSWAP_CORE
-	ZRAM_BATCHING_OUT,
-	ZRAM_FROM_HYBRIDSWAP,
-	ZRAM_MCGID_CLEAR,
-	ZRAM_IN_BD, /* zram stored in back device */
-#endif
+	ZRAM_DEDUPED,	/* Deduplicated with existing entry */
 	__NR_ZRAM_PAGEFLAGS,
 };
 
