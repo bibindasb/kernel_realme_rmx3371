a808b28880a7c9c90f29af62489d734780578a78
jabashque <jabashque@gmail.com>
2022-11-18 15:49:58 +0000

    touchscreen: Fix errors due to assumption of CONFIG_FB always being set
    oplus_touchscreen's touchpanel_common.h's touchpanel_data struct
    contains the `fb_notif` field only if CONFIG_FB is set. However, that
    field gets used to register as notifier clients for both fb and msm_drm.
    Fix this by letting this field appear if either CONFIG_FB or
    CONFIG_MSM_DRM is set instead.
    
    oplus_touchscreen's touchpanel_common_driver.c's fb_notifier_callback()
    would cast the incoming data pointer to a `struct fb_event *`,
    regardless of whether this callback was registered for fb or msm_drm.
    Fix this by having it cast to a `struct msm_drm_notifier*` instead if
    CONFIG_DRM_MSM is defined.
    
    All the oplus touchscreen drivers populated their dev_pm_ops structs
    with suspend/resume functions only if CONFIG_FB is set. Fix this by
    having the preprocessor check for CONFIG_FB or CONFIG_DRM_MSM instead.
    
    synaptics_dsx's synaptics_dsx_rmi_dev.c calls send_sig_info(), but if
    CONFIG_FB isn't set, then the implicit linux/sched/signal.h include gets
    lost. Explicitly include linux/sched/signal.h to fix this.
    
    Change-Id: Ibe00a070b57eac78f3492603d73b4869d099a9d9

--- drivers/input/touchscreen/oplus_touchscreen/Focal/FT8006/focal_drivers_ft8006.c
+++ drivers/input/touchscreen/oplus_touchscreen/Focal/FT8006/focal_drivers_ft8006.c
@@ -2354,7 +2354,7 @@ static struct of_device_id tp_match_table[] =
 };
 
 static const struct dev_pm_ops tp_pm_ops = {
-#ifdef CONFIG_FB
+#if defined(CONFIG_FB) || defined(CONFIG_DRM_MSM)
     .suspend = focal_i2c_suspend,
     .resume = focal_i2c_resume,
 #endif
